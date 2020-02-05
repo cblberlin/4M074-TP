d7dd294083075c29535fb0a29d6a116959f1f367
cblberlin <bailin.cai@hotmail.fr>
2020-02-05 06:51:26 +0100

    Update TP1.R

--- TP1/TP1.R
+++ TP1/TP1.R
@@ -1,3 +1,13 @@
-n <- 1000
+# reproduire 3 graphes(Histogram, pairs adjacents, autocorrelation function)
+n <- 10000
 
-unif <- runif(n, min = 0, max = 1)
+x <- runif(n, min = 0, max = 1)
+
+par(mfrow = c(1,3))
+hist(x, main = "Histogram of uniform distribution", ylab = NA)
+xn <- x[-n] # delete the last element
+xnl <- x[-1] # delete the first element
+plot(xn, xnl, main = "Graphe of pairs adjacents", ylab = NA, xlab = NA) # graphe adjacent pair
+acf(x, main = "autocorrelation function", ylab = NA)
+
+# reproduire 2 graphes
